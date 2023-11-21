af9d2b015a729e1abd5bc38b2e6bd08a873cde18
EricaHu <xihu4221@uni.sydney.edu.au>
2023-11-21 09:41:39 +0800

    random num from 1 to 6

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,27 @@ import { useState } from "react";
 import "./App.scss";
 import Die from "./components/Die";
 
+/**
+ * Challenge:
+ *
+ * Write a function (allNewDice) that returns an array
+ * of 10 random numbers between 1-6 inclusive.
+ *
+ * Log the array of numbers to the console for now
+ */
 
 function App() {
     const [count, setCount] = useState(0);
+
+    function allNewDice() {
+        let arr = [];
+        for (let i = 0; i < 10; i++) {
+            arr.push(Math.floor(Math.random() * 6) + 1);
+        }
+        return arr;
+    }
+
+    console.log(allNewDice());
 
     return (
         <>
